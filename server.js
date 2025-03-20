e37529767bf536b07d04d2cb2c0abe06a8244974
Volodymyr Pichkov <volodymyr2000pichkov@gmail.com>
2025-03-20 15:45:35 +0200

    server v 2.2

--- server.js
+++ server.js
@@ -9,29 +9,29 @@ const users = Array.from({ length: 20 }, (_, index) => ({
   age: 22 + index,
 }));
 
-fs.writeFile("user.txt", JSON.stringify(users, null, 1), (err) => {
+fs.writeFile("users.txt", JSON.stringify(users, null, 1), (err) => {
   if (err) {
     console.error(err);
   } else {
-    console.log("Файл user.txt успешно создан");
+    console.log("Файл users.txt успешно создан");
   }
 });
 
 const server = http.createServer((req, res) => {
   if (req.method === "GET") {
-    fs.readFile("user.txt", "utf8", (err, data) => res.end(err || data));
+    fs.readFile("users.txt", "utf8", (err, data) => res.end(err || data));
   } else if (req.method === "POST") {
     let body = "";
     req.on("data", (chunk) => (body += chunk));
     req.on("end", () =>
-      fs.appendFile("user.txt", body + "\n", () => res.end("OK"))
+      fs.appendFile("users.txt", body + "\n", () => res.end("OK"))
     );
   } else if (req.method === "DELETE") {
-    fs.readFile("user.txt", "utf8", (err, data) => {
+    fs.readFile("users.txt", "utf8", (err, data) => {
       const updatedUsers = JSON.parse(data).filter(
-        (user) => user.id != req.url.split("/")[1]
+        (users) => users.id != req.url.split("/")[1]
       );
-      fs.writeFile("user.txt", JSON.stringify(updatedUsers, null, 1), (err) =>
+      fs.writeFile("users.txt", JSON.stringify(updatedUsers, null, 1), (err) =>
         res.end(err || "Пользователь удален")
       );
     });
